9bfed27a3218436caaed3cfd49845b1087b19359
VinkarioDev <rogeriobuenodev@gmail.com>
2026-06-30 16:39:21 -0300

    "style markup task "

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,41 @@ import "./App.css"
 import { ButtonAdd } from "./components/ButtonAdd"
 import { InputTask } from "./components/InputTask"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTrash } from "@fortawesome/free-solid-svg-icons" 
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
 
 function App() {
   const [task, setTask] = useState([])
   const [inputValue, setInputValue] = useState("")
   const [error, setError] = useState(false)
+
   function addTask() {
-    if (inputValue === "") {
+    if (inputValue.trim() === "") {
       setError(true)
       return
     } else {
       setError(false)
-      const tmp = [...task, inputValue]
-      setTask(tmp)
+      const newTask = {
+        id: Date.now(),
+        texto: inputValue,
+        concluida: false
+      }
+      setTask([...task, newTask])
       setInputValue("")
     }
   }
-  function deleteTask(index) {
-    setTask(() => {
-      const tmp = [...task]
 
-      return tmp.filter((_, indexAtual) => indexAtual !== index)
-    })
+  function deleteTask(idDeletar) {
+    setTask(task.filter(task => task.id !== idDeletar))
   }
+
+  function markupCheck(idAlternar) {
+    setTask(
+      task.map(task =>
+        task.id === idAlternar ? { ...task, concluida: !task.concluida } : task
+      )
+    )
+  }
+
   return (
     <div className="container">
       <h1>Lista de Tarefas</h1>
@@ -43,24 +54,32 @@ function App() {
             textBtn={"Add"}
           />
         </div>
-        <ul >
+        <ul>
           {
-            task.map((task, index) => {
+            task.map((task) => {
               return (
-                <div className="listTask" key={index}>
-                  <li >{task}</li>
-                  <button  onClick={() => deleteTask(index)}>
-                  <FontAwesomeIcon style={{color:"#fff"}} icon={faTrash} />
+                <div className="listTask" key={task.id}>
+                  <li>
+                    {task.concluida ? <del style={{ textDecorationColor: "#ca9c17", fontStyle: "italic" }}>{task.texto}</del> : task.texto}
+                  </li>
 
-                  </button>
-
+                  <div className="inputCheck">
+                    <input
+                      className="checkbox"
+                      type="checkbox"
+                      checked={task.concluida}
+                      onChange={() => markupCheck(task.id)}
+                    />
+                    <button onClick={() => deleteTask(task.id)}>
+                      <FontAwesomeIcon style={{ color: "#1b718a" }} icon={faTrash} />
+                    </button>
+                  </div>
                 </div>
               )
             })
           }
         </ul>
       </div>
-
     </div>
   )
 }
